f70ba7f822a62edb3e96fb29f728131a12475eb6
Denis Piyalkin <pialkind@mail.ru>
2023-09-19 15:45:38 +0300

    Added userdata GET request handler

--- src/pages/api/users/[id].js
+++ src/pages/api/users/[id].js
@@ -1,9 +1,22 @@
+import { MongoClient } from 'mongodb';
+
 export default async function handler(req, res) {
-    const { id } = req.query;
+    const client = new MongoClient(process.env.MONGODB_URI, { });
+
+    try {
+        await client.connect();
+
+        const { id } = req.query;
+
+        //// Поменять поля, если не совпадают с полями в монге:
+        const user = await req.db.collection('users').findOne({ _id: ObjectId(id) }); 
     
-    const response = await fetch('https://jsonplaceholder.typicode.com/users/${id}');
-  
-    const userData = await response.json();
-  
-    res.status(200).json(userData);
+        if (!user) {
+          return res.status(404).json({ message: 'User not found' });
+        }
+    
+        return res.status(200).json(user);
+      } catch (error) {
+        return res.status(500).json({ message: 'Internal server error' });
+      }
 }
