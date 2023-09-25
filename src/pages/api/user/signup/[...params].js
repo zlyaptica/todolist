673e7275ba1a5124c2fd0b5db28a9d96ec4e1144
Ilia Shimozerov <ilia.shimozerov@gmail.com>
2023-09-25 17:24:22 +0300

    Added unique user check condition in /api/user/signup API call

--- src/pages/api/user/signup/[...params].js
+++ src/pages/api/user/signup/[...params].js
@@ -10,11 +10,13 @@ export default async function handler(req, res) {
         const name = params[0];
         const nickname = params[1];
         const password = params[2];
+        const check_user = await client.db('ToDoListApp').collection('Users').findOne({ "nickname": nickname });
+        if (check_user) return res.status(404).json({ message: 'This user already exists' });
         const inserted = await client.db('ToDoListApp').collection('Users').insertOne({ "name": name, "nickname": nickname, "password": password, "boards": []  });
         client.close();
         if (!inserted)
         {
-          return res.status(404).json({ message: 'Oops! User has not been added!' });
+          return res.status(404).json({ message: 'User has not been added' });
         }
         return res.status(200).json({ message: 'Successful sign up' });
             
